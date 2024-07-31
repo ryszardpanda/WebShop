8de4bd928c677ee86ea02026e5da014c2f202bed
Ryszard Różański <rychu48@op.pl>
2024-08-01 00:06:57 +0200

    Task1 - Done - Stworzona klasa bazowa Product, Klasy dziedziczonce (Computer, Smarthpone oraz Electronics). Dodatkowo klasy typu Enum przechowujące informację potrzebne do personalizacji produktów i zakupu akcesoriów. Zostały również zaimplementowane metody pozlwające na konfiguracje produktów.
    LGTM! SEND IT! :P

---
+++ src/Computer.java
@@ -0,0 +1,30 @@
+//**Typ Produktu: Komputer**
+//
+//**Specyficzna Obsługa**: Implementacja funkcji umożliwiającej konfigurację specyfikacji laptopa/komputera przed dodaniem
+// do koszyka, takiej jak wybór procesora, ilości RAM, itp.
+
+import enums.Processor;
+import enums.RAM;
+
+public class Computer extends Product{
+    private Processor processor;
+    private RAM ram;
+    private int storage;
+
+    public Computer(int id, String name, double price, int availableQuantity) {
+        super(id, name, price, availableQuantity);
+    }
+
+    public void configureComputer( Processor processor, RAM ram, int storage) {
+        this.processor = processor;
+        this.ram = ram;
+        this.storage = storage;
+        System.out.println("Skonfigurowano komputer: Procesor: " + processor + ", RAM: " + ram + "GB, Dysk: " + storage + "GB");
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Procesor: " + processor + ", RAM: " + ram + "GB, Dysk: " + storage + "GB");
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,73 @@
+////**Wymagania Biznesowe**
+////**Task 1**: Stworzenie klasy Product reprezentującej produkt z polami takimi jak id, nazwa, cena, ilość dostępna. - DONE
+//W sklepie internetowym sprzedawane będą produkty będące elektroniką:
+//
+//**Typ Produktu: Komputer**
+//
+//**Specyficzna Obsługa**: Implementacja funkcji umożliwiającej konfigurację specyfikacji laptopa/komputera przed dodaniem
+// do koszyka, takiej jak wybór procesora, ilości RAM, itp. - DONE
+//
+//**Typ Produktu: Smartfon**
+//
+//Specyficzna Obsługa: Dodanie funkcji umożliwiającej wybór koloru, pojemności baterii,
+// oraz dodatkowych akcesoriów przy zakupie smartfona. - DONE
+//
+//**Dla pozostałych produktów, które będą sprzedawane w sklepie typem produktu będzie po prostu:
+// Electronics i dla produktów o tym typie nie ma żadnej dodatkowej obsługi!!!** - DONE
+//
+//**CEL:**
+//Dostosowanie obsługi różnych typów produktów pozwala na lepsze spełnienie oczekiwań klientów.
+// Funkcje specyficzne dla każdego typu produktu mogą zwiększyć atrakcyjność sklepu i zapewnić klientom bardziej
+// spersonalizowane doświadczenie zakupowe.
+
+//DONE
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int availableQuantity;
+
+    public Product(int id, String name, double price, int availableQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public void displayDetails() {
+        System.out.println("Produkt: " + name + ", Cena: " + price + ", Dostępna ilość: " + availableQuantity);
+    }
+}
