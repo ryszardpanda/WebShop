b2efd72b344ca3f786ce41e4a290b24b568e70ce
Ryszard Różański <rychu48@op.pl>
2024-08-05 23:01:16 +0200

    Task 3-5 DONE, Class: Cart, Order, Customer

---
+++ src/Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+    private String name;
+    private String email;
+    private String adress;
+
+    public Customer(String name, String email, String adress) {
+        this.name = name;
+        this.email = email;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
